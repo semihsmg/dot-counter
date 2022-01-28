a7d7c7b420f09529b09e4dcc7c04253db5149225
Semih <23503571+semihsmg@users.noreply.github.com>
2022-01-28 03:29:24 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import cv2
 import numpy
 
 cv2.namedWindow("output", cv2.WINDOW_NORMAL)
-
 img = cv2.imread('Monocyte_40x.JPG', 0)
 
 # https://docs.opencv.org/4.x/dd/d1a/group__imgproc__feature.html#ga47849c3be0d0406ad3ca45db65a25d2d
@@ -12,7 +11,6 @@ circles = cv2.HoughCircles(img, cv2.HOUGH_GRADIENT, dp=1, minDist=30, param1=150
 
 if circles is not None:
     circles = numpy.round(circles[0, :]).astype("int")
-
     index = 0
     for (x, y, r) in circles:
         cv2.circle(img, (x, y), r, (255, 255, 0), 2)
@@ -20,14 +18,7 @@ if circles is not None:
         index = index + 1
 
     print('No. of circles detected = {}'.format(index))
-
-    # outSized = cv2.resize(output, (950, 950))
-    # outSized = cv2.resize(img, (950, 950))
-    # imgSized = cv2.resize(img, (950, 950))
-
     cv2.imshow("output", img)
-    # cv2.imshow("output", numpy.hstack([imgSized, outSized]))
-
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 else:
